75371f7d9382fd3dc87d8289caa1ffbb9bcc79ac
andarutr <andarutr@gmail.com>
2023-02-17 09:25:56 +0700

    Day 2 : Membuat Halaman Administrator

--- application/controllers/administrator/dashboard.php
+++ application/controllers/administrator/dashboard.php
@@ -1,10 +1,25 @@
 <?php 
 
 class Dashboard extends CI_Controller{
+	public function __construct(){
+		parent::__construct();
+
+		if(!isset($this->session->userdata['username'])){
+			$this->session->set_flashdata('pesan','<div class="alert alert-info">Login Dulu!</div>');
+			redirect('administrator/auth');
+		}
+	}
+
 	public function index(){
+		$data = $this->user_model->ambil_data($this->session->userdata('username'));
+		$data = array(
+				'username' => $data->username,
+				'level' => $data->level
+		);
+		
 		$this->load->view('templates_administrator/header');
 		$this->load->view('templates_administrator/sidebar');
-		$this->load->view('administrator/dashboard');
+		$this->load->view('administrator/dashboard', $data);
 		$this->load->view('templates_administrator/footer');
 	}
 }
--- application/views/administrator/dashboard.php
+++ application/views/administrator/dashboard.php
@@ -1,99 +1,88 @@
-<!-- Begin Page Content -->
 <div class="container-fluid">
-
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+    <h4>
+        <i class="fas fa-tachometer-alt"></i> Dashboard
+    </h4>
+    <div class="alert alert-success">
+        <h4 class="alert-heading">Selamat Datang!</h4>
+        <p>Selamat datang <?= $username ?> di Sistem Informasi Akademik Universitas Bina Sarana Informatika.</p>
+        <p>Anda Login Sebagai : <?= $level ?></p>
+        <hr>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-sm btn-info" data-toggle="modal" data-target="#exampleModal">
+          <i class="fas fa-cogs"></i>Control Panel
+        </button>
     </div>
 
-    <!-- Content Row -->
-    <div class="row">
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Earnings (Monthly)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-cogs"></i>Control Panel</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">MAHASISWA</p></a>
+                    <i class="fas fa-3x fa-user-graduate"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">TAHUN AKADEMIK</p></a>
+                    <i class="fas fa-3x fa-calendar-alt"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">KRS</p></a>
+                    <i class="fas fa-3x fa-edit"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">KHS</p></a>
+                    <i class="fas fa-3x fa-file-alt"></i>
+                </div>
+            </div><hr>
+            <div class="row">
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">INPUT NILAI</p></a>
+                    <i class="fas fa-3x fa-sort-numeric-down"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">CETAK TRANSKRIP</p></a>
+                    <i class="fas fa-3x fa-print"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">KATEGORI</p></a>
+                    <i class="fas fa-3x fa-list-ul"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">INFO KAMPUS</p></a>
+                    <i class="fas fa-3x fa-bullhorn"></i>
+                </div>
+            </div><hr>
+            <div class="row">
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">IDENTITAS</p></a>
+                    <i class="fas fa-3x fa-id-card-alt"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">TENTANG KAMPUS</p></a>
+                    <i class="fas fa-3x fa-info-circle"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">FASILITAS</p></a>
+                    <i class="fas fa-3x fa-laptop"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?= base_url() ?>"><p class="nav-link small text-info">GALLERY</p></a>
+                    <i class="fas fa-3x fa-images"></i>
                 </div>
             </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+          </div>
         </div>
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Earnings (Annual)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                            </div>
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                </div>
-                                <div class="col">
-                                    <div class="progress progress-sm mr-2">
-                                        <div class="progress-bar bg-info" role="progressbar"
-                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                            aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Pending Requests</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-    
 </div>
-<!-- /.container-fluid -->
